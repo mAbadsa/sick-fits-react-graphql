306698e54e40cf44415523c90900ced7577cd874
mAbadsa <moh22389@gmail.com>
2020-10-29 12:00:45 +0200

    Upload Files

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+import Link from 'next/link';
+
+function index(props) {
+    return (
+        <div>
+            <p>Hey!</p>
+        </div>
+    );
+}
+
+export default index;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+function Sell(props) {
+    return (
+        <div>
+            <h3>Sell Page</h3>
+        </div>
+    );
+}
+
+export default Sell;
